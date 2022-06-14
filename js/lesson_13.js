7b8665d9f8702b8145633edafe02ea9a9e2517ed
SashikFox <kharkovpc@gmail.com>
2022-06-14 14:58:09 +0300

    add lesson 13

---
+++ js/lesson_13.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(String(null));
+console.log(typeof(String(4)));
+
+console.log(typeof(null + ''));
+
+const num =5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+console.log(typeof(Number('4')));
+
+console.log(typeof(+'6'));
+
+console.log(parseInt("15px",10));
+
+console.log(typeof(!!'koo!'));
